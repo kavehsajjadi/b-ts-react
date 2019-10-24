2b05564ca7407a970832fbc22da704043dd0f361
Kaveh Sajjadi <kaveh.sajjadi@gmail.com>
2019-10-25 00:12:23 +1100

    make up/down arrows work

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -2,18 +2,19 @@ import * as React from "react"
 import { Command } from "lib/commands"
 
 export const Dropdown = ({
-  commands,
+  items,
   onClick,
 }: {
-  commands: Command[]
+  items: { command: Command; ref: React.RefObject<DropdownItem> }[]
   onClick(c: Command): void
 }) => (
   <DropdownList>
-    {commands.map(command => (
+    {items.map(({ command, ref }) => (
       <DropdownItem
         key={command[0]}
         onClick={() => onClick(command)}
         label={command[0]}
+        ref={ref}
       />
     ))}
   </DropdownList>
@@ -28,14 +29,32 @@ const DropdownList = ({ children }) => (
   </div>
 )
 
-const DropdownItem = ({ label, onClick }) => (
-  <div className="dropdown-item">
-    <button
-      type="button"
-      onClick={onClick}
-      style={{ background: "none", border: "none" }}
-    >
-      {label}
-    </button>
-  </div>
-)
+export class DropdownItem extends React.Component<{
+  label: string
+  onClick(e: React.MouseEvent<HTMLButtonElement>): void
+}> {
+  buttonRef = React.createRef<HTMLButtonElement>()
+
+  focus() {
+    if (this.buttonRef.current != null) {
+      this.buttonRef.current.focus()
+    }
+  }
+
+  render() {
+    const { label, onClick } = this.props
+
+    return (
+      <div className="dropdown-item">
+        <button
+          ref={this.buttonRef}
+          type="button"
+          onClick={onClick}
+          style={{ background: "none", border: "none" }}
+        >
+          {label}
+        </button>
+      </div>
+    )
+  }
+}
